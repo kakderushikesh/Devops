9718da0429707ecf7b3ade684869070979d8628c
kakderushikesh <rushikeshkakde789@gmail.com>
2023-03-08 13:17:03 +0530

    UPDATE 2

--- Factorial.java
+++ Factorial.java
@@ -1,7 +1,7 @@
 public class Factorial
 {
     public static void main(String[] args)
-    {	final int NUM_FACTS = 5;
+    {	final int NUM_FACTS = 3;
         for(int i = 0; i < NUM_FACTS; i++)
             System.out.println( i + "! is " + factorial(i));
     }
